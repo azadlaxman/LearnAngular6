8f28451eb1fb5f6971a9d3da3d159ccff86c1261
Azad Laxman <azad.laxman@honeywell.com>
2018-08-24 10:54:57 +0530

    Seed commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { LayoutComponent } from './ui/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiModule
   ],
-  providers: [],
+  providers: [LayoutComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
